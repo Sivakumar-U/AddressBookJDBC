801fc78fc803624d8d1e26272561f96fc4229a86
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-22 18:03:02 +0530

    Added: Test case to count all the entries from database

---
+++ src/test/java/com/blz/addressbook/jdbc/AddressBookTest.java
@@ -0,0 +1,27 @@
+package com.blz.addressbook.jdbc;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.blz.addressbook.jdbc.AddressBookService.IOService;
+
+public class AddressBookTest {
+
+	static AddressBookService addressBookService;
+
+	@BeforeClass
+	public static void createAddressBookObj() {
+		addressBookService = new AddressBookService();
+	}
+
+	@Test
+	public void givenAddressBookContactsInDB_WhenRetrieved_ShouldMatchContactsCount() throws AddressBookException {
+		List<AddressBookData> addressBookData = addressBookService.readAddressBookData(IOService.DB_IO);
+		Assert.assertEquals(5, addressBookData.size());
+	}
+}
